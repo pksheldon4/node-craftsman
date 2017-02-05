feafe0c817064f1c1b6f61d843918cc2f0496ece
pksheldon4 <pksiv@hotmail.com>
2017-02-05 08:00:04 -0500

    Remove migrations from .gitignore

---
+++ keyword-wrangler/migrations/20170204185557-createKeywordAndCategoryTable.js
@@ -0,0 +1,32 @@
+var dbm = global.dbm || require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  async.series(
+      [
+          db.createTable.bind(db, 'keyword', {
+            id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true},
+            value: {type: 'string', length: 128, notNull:true, unique: true},
+            categoryID: {type: 'int', notNull: true}
+
+          }),
+
+          db.createTable.bind(db, 'category', {
+              id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true},
+              name: {type: 'string', length: 128, notNull:true, unique: true}
+          })
+      ],
+      callback);
+
+};
+
+exports.down = function(db, callback) {
+
+  async.series(
+      [
+          db.dropTable.bind(db, 'keyword'),
+          db.dropTable.bind(db, 'category')
+      ],
+      callback);
+
+};
